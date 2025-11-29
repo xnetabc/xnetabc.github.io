25ee4463bb6a101b98d600ac8f1234c06c289f60
xingang.li <windgorain@163.com>
2025-11-29 19:45:04 +0800

    update

--- js/my/copyright.js
+++ js/my/copyright.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-	$('#copyright').html( '<div class="Container"><hr><p align="center"> ∞Ê»®À˘”–: VNINI.COM </p><p align="center"> VNINI.COM Copyright 2013-2016 All rights reserved</p><br><br></div>');
+	$('#copyright').html( '<div class="Container"><hr><p align="center"> <p align="center"> xnets.top </p><br><br></div>');
 });
 
 
